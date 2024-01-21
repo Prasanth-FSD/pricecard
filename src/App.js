3d5d0500404ee833029367066ae4d272bbf87888
PRASANTH D <chinnakutty1470@gmail.com>
2024-01-21 22:28:34 +0530

    initial summit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './ProductCard';
+import ProductCard from './ProductCard';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/font-awesome/css/font-awesome.min.css'
+import ProductCard2 from './ProductCard2';
+import ProductCard3 from './ProductCard3';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-4 col-lg-6 col-sm-12 col-xl-4 col-xxl-4 col-md-6">
+            <ProductCard plan="FREE" month="$0/month"/>
+          </div>
+          <div className="col-sm-4 col-lg-6 col-sm-12 col-xl-4 col-xxl-4 col-md-6">
+            <ProductCard2 plan="PLUS" month="$9/month"/>
+          </div>
+          <div className="col-sm-4 col-lg-6 col-sm-12 col-xl-4 col-xxl-4 col-md-6">
+            <ProductCard3 plan="PRO" month="$49/month"/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
